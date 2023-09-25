2bfb3c162e3ed8dbbdd8d83bf0ec5722bba1e6ea
Alexander Valencia <alex16jpv@gmail.com>
2023-09-24 21:24:22 -0500

    feat: update color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#212121",
+        secondary: "#323232",
+        "primary-light": "#0D7377",
+        "secondary-light": "#14FFEC",
+      },
+    },
   },
   plugins: [],
+  darkMode: "media",
 };
