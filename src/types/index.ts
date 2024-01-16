eb3decb16eac479fe17bf5cd358b9373c89e8f18
Alexander Valencia <alex16jpv@gmail.com>
2024-01-15 21:53:11 -0500

    Refactor portfolio component and add PortfolioGallery

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,12 @@
 export interface ImagesDictionary {
   [key: string]: any;
 }
+
+export type ProjectType = {
+  image: string | any;
+  isWIP: boolean;
+  title: string;
+  description: string;
+  link: string;
+  repository: string;
+};
