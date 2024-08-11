29f245400f80a6775615013eaf46343fcdf0201f
Alexander Valencia <alex16jpv@gmail.com>
2024-08-10 19:00:05 -0500

    chore: Update static.yml and astro.config.mjs for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+const isGitHubPages = process.env.SITE_ENV === "github";
+const site = isGitHubPages
+  ? "https://test.github.io"
+  : "https://www.alexconlag.com";
+const base = isGitHubPages ? "/alex16jpv/" : "/";
+
 export default defineConfig({
-  site: "https://www.alexconlag.com",
+  site,
+  base,
   integrations: [tailwind(), sitemap()],
 });
